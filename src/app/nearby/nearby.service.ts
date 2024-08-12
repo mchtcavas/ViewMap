512230ed46f8e70f3711f8752f4c94939cdcf9fc
Mücahit Çavaş <mchtcavas@gmail.com>
2024-08-12 22:40:17 +0300

    Implemented search bar functionality for filtering nearby places

--- src/app/nearby/nearby.service.ts
+++ src/app/nearby/nearby.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Nearby } from '../models/nearby.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NearbyService {
-  private BASE_URL: string = "https://mocki.io/v1/e2f504be-c5b6-475a-8d1e-7ade2ff8aca3";
+  private BASE_URL: string = "http://localhost:8070/api/nearby";
 
   constructor(private httpClient: HttpClient) { }
 
   getNearby(latitude: number, longitude: number, radius: number): Observable<Nearby[]> {
-    return this.httpClient.get<Nearby[]>(this.BASE_URL);
+    const params = new HttpParams()
+      .set('latitude', latitude.toString())
+      .set('longitude', longitude.toString())
+      .set('radius', radius.toString());
+
+    return this.httpClient.get<Nearby[]>(this.BASE_URL, { params });
   }
 }
